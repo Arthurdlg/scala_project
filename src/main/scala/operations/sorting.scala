8ff52d42e4d54f5b9335372b81b9796f6bba485a
AIphahs <aurelien.ah-sane@efrei.net>
2024-07-03 11:39:24 +0200

    Adding two operations : Floyd, Djikstra, -not actually working

---
+++ src/main/scala/operations/sorting.scala
@@ -0,0 +1,18 @@
+package operations
+
+
+def topologicalSort[V](graph: Graphs.Graph[V, _]): List[V] = {
+  def visit(node: V, visited: Set[V], stack: List[V]): (Set[V], List[V]) = {
+    if (visited.contains(node)) (visited, stack)
+    else {
+      val (newVisited, newStack) = graph.neighbors(node).foldLeft((visited, stack)) {
+        case ((vis, st), neighbor) => visit(neighbor, vis, st)
+      }
+      (newVisited + node, node :: newStack)
+    }
+  }
+  val (visited, stack) = graph.vertices.foldLeft((Set[V](), List[V]())) {
+    case ((vis, st), node) => visit(node, vis, st)
+  }
+  stack.reverse
+}
