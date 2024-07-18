3c4d82c1fb86ca825a599b97b121bb5613b473f6
AIphahs <aurelien.ah-sane@efrei.net>
2024-07-18 18:20:39 +0200

    Update project
    GraphVizSupport
    JsonSupport
    GraphUnitTest
    JsonUnitTest

---
+++ src/test/scala/fr/efrei/graphslibrary/unittest/utils/GraphVizSupportUnitTest.scala
@@ -0,0 +1,30 @@
+package fr.efrei.graphslibrary.utils
+
+import fr.efrei.graphslibrary.graphs.Graph
+import fr.efrei.graphslibrary.edges.Edge
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class GraphVizSupportSpec extends AnyFlatSpec with Matchers {
+  "GraphVizSupport" should "convert a graph to GraphViz format" in {
+    // Mock classes for testing
+    case class TestEdge(node1: String, node2: String) extends Edge[String]
+    case class TestGraph(vertices: Set[String], edges: Set[TestEdge]) extends Graph[String, TestEdge] {
+      def getAllVertices: Set[String] = vertices
+      def getAllEdges: Set[TestEdge] = edges
+      def neighbors(vertex: String): Set[String] = Set()
+      def addEdge(edge: TestEdge): Graph[String, TestEdge] = this
+      def removeEdge(edge: TestEdge): Graph[String, TestEdge] = this
+    }
+
+    val graph = TestGraph(Set("A", "B"), Set(TestEdge("A", "B")))
+
+    val graphViz = GraphVizSupport.toGraphViz(graph)
+
+    graphViz should include ("digraph G {")
+    graphViz should include ("  \"A\";")
+    graphViz should include ("  \"B\";")
+    graphViz should include ("  \"A\" -> \"B\";")
+    graphViz should include ("}")
+  }
+}
