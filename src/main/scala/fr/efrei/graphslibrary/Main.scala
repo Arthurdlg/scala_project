23170fe77cfc7bfd7b39cb55a594531af08735f0
Arthurdlg <114220198+Arthurdlg@users.noreply.github.com>
2024-07-19 02:08:30 +0200

    issue with dependecies + zio app for weighted graph

---
+++ src/main/scala/fr/efrei/graphslibrary/Main.scala
@@ -0,0 +1,10 @@
+package fr.efrei.graphslibrary
+
+import fr.efrei.graphslibrary.edges.DirectedEdge
+import fr.efrei.graphslibrary.graphs.DirectedGraph
+import fr.efrei.graphslibrary.operations.FloydWarshall
+
+@main
+def main(): Unit = {
+  
+}
